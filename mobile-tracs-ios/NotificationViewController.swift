77a63c5908d54f6cd11fab7213e167f4f87fc664
wickning1 <git@mythgaming.net>
2017-03-20 18:47:50 -0500

    added a tableview to the notification screen

--- mobile-tracs-ios/NotificationViewController.swift
+++ mobile-tracs-ios/NotificationViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class NotificationViewController: UIViewController {
+class NotificationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.hidesBackButton = true
+        //navigationItem.hidesBackButton = true
+        tableView.register(UINib(nibName:"NotificationCell", bundle: nil), forCellReuseIdentifier: "notification")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,15 +22,20 @@ class NotificationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "notification", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        cell.detailTextLabel?.text = "Test"
+        return cell
+    }
+    
 }

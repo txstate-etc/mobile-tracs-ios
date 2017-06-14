d8f85d750a135f79ae9b62ed18f8b631c1d52aea
wickning1 <git@mythgaming.net>
2017-06-14 17:10:09 -0500

    added intro screen in a wkwebview

--- mobile-tracs-ios/IntroViewController.swift
+++ mobile-tracs-ios/IntroViewController.swift
@@ -7,32 +7,43 @@
 //
 
 import UIKit
+import WebKit
 
-class IntroViewController: UIViewController {
-    @IBOutlet var okButton:UIButton!
+class IntroViewController: UIViewController, UIGestureRecognizerDelegate {
+    @IBOutlet var webView:WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        okButton.addTarget(self, action: #selector(okPressed), for: .touchUpInside)
-        // Do any additional setup after loading the view.
+        webView = Utils.getWebView()
+        webView.scrollView.minimumZoomScale = 1.0
+        let taprecognizer = UITapGestureRecognizer(target: self, action: #selector(okPressed))
+        taprecognizer.delegate = self
+        webView.scrollView.addGestureRecognizer(taprecognizer)
+        view.addSubview(webView)
+        Utils.constrainToContainer(view: webView, container: view)
+        let htmlpath = Bundle.main.path(forResource: "welcome_page", ofType: "html")
+        let folderpath = Bundle.main.bundlePath
+        let baseurl = URL(fileURLWithPath: folderpath, isDirectory: true)
+        do {
+            let htmlstring = try NSString(contentsOfFile: htmlpath!, encoding: String.Encoding.utf8.rawValue)
+            webView.loadHTMLString(htmlstring as String, baseURL: baseurl)
+        } catch {
+            NSLog("IntroViewController could not load html!")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        webView.scrollView.zoomScale = 1.0
+        webView.evaluateJavaScript("document.body.style.zoom = 1.0", completionHandler: nil)
     }
     
     func okPressed() {
         self.dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - UIGestureRecognizerDelegate
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
-    */
-
 }
